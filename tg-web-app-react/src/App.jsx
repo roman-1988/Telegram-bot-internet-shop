d49e7d12fea81935fa3f8a82bdf21fa0f6cc5037
Roman <roman-1@inbox.ru>
2023-07-10 21:55:58 +0300

    Подключили телеграмовский скрипт в файл index.html. В файле App.css добавили специальные стили от этого api. В компоненте App.jsx созадали кнопку 'Закрыть' и добавили специальные телеграмовские функции в хук useEffect и обработчик на закрытие.

--- tg-web-app-react/src/App.jsx
+++ tg-web-app-react/src/App.jsx
@@ -1,10 +1,21 @@
+import { useEffect } from "react"
 import "./App.css"
+const tg = window.Telegram.WebApp
 
 const App = () => {
+  useEffect(() => {
+    tg.ready()
+  }, [])
+
+  const onClose = () => {
+    tg.close()
+  }
 
   return (
     <>
       <h1>Vite + React</h1>
+
+      <button onClick={onClose}>Закрыть</button>
     </>
   )
 }
