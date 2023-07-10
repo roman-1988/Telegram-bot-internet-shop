f3dfe92f1f33c4c4aa9c1195aebdf48e5f962983
Roman <roman-1@inbox.ru>
2023-07-10 19:58:12 +0300

    Созал ботовскую кнопку из api telegram в файле index.js. В папке для фронтенда очистил не нужные файлы и подготовил главный компонент App.jsx, добавил базовые стили в App.css.

--- tg-web-app-bot/index.js
+++ tg-web-app-bot/index.js
@@ -1,19 +1,21 @@
 const TelegramBot = require("node-telegram-bot-api")
 
 const token = "5834354543:AAFbJBab2H4dpFg2bx0qvWKXo_i1WeCojjo"
+const webAppUrl = "/"
 
 const bot = new TelegramBot(token, { polling: true })
 
-// bot.onText(/\/echo (.+)/, (msg, match) => {
-
-//     const chatId = msg.chat.id
-//     const resp = match[1]
-
-//     bot.sendMessage(chatId, resp)
-// })
-
-bot.on("message", (msg) => {
+bot.on("message", async (msg) => {
     const chatId = msg.chat.id
+    const text = msg.text
 
-    bot.sendMessage(chatId, "Received your message")
+    if (text === "/start") {
+        await bot.sendMessage(chatId, "Ниже появится кнопка, заполни форму", {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: "Сделать заказ", web_app: { url: webAppUrl } }]
+                ]
+            }
+        })
+    }
 })
