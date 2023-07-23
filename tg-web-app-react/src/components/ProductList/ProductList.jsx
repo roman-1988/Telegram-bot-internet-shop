07096acefbb7254a2e3dcf6c02458a1179793c79
Roman <roman-1@inbox.ru>
2023-07-23 17:11:45 +0300

    Создал функцию onAdd которая осуществляет логику по подсчету товаров и т.д. - т.е. несет функцию корзины

--- tg-web-app-react/src/components/ProductList/ProductList.jsx
+++ tg-web-app-react/src/components/ProductList/ProductList.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+import { useTelegram } from "../hooks/useTelegram"
 import { ProductItem } from "../ProductItem/ProductItem"
 import "./ProductList.css"
 
@@ -12,7 +14,38 @@ const products = [
     { id: "8", title: "Куртка 5", price: 12000, description: "Зеленого цвета, теплая" },
 ]
 
+const getTotalPrice = (items = []) => {
+    return items.reduce((acc, item) => {
+        return acc += item.price
+    }, 0)
+}
+
 const ProductList = () => {
+    const [addedItems, setAddedItems] = useState([])
+    const { tg } = useTelegram()
+
+    const onAdd = (product) => {
+        const alreadyAdded = addedItems.find(item => item.id === product.id)
+        let newItems = []
+
+        if (alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id)
+        } else {
+            newItems = [...addedItems, product]
+        }
+
+        setAddedItems(newItems)
+
+        if (newItems.length === 0) {
+            tg.MainButton.hide()
+        } else {
+            tg.MainButton.show()
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            })
+        }
+    }
+
     return (
         <div className={"list"}>
             {products.map(item => (
