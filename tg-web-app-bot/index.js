c1c6d48e93d8c53ef2edaf1fe2be67eeaacae33e
Roman <roman-1@inbox.ru>
2023-07-24 20:52:54 +0300

    В хук useTelegram добавили телеграмоский параметр queryId. В компоненте ProductList добавили POST запрос с данными. Установили две зависимости - библиотеку Express и Cors. POST запрос добавили и на бэкенде с проверкой об успешной или не успешной покупке, а также создали сервер на 8000 порте.

--- tg-web-app-bot/index.js
+++ tg-web-app-bot/index.js
@@ -1,9 +1,15 @@
 const TelegramBot = require("node-telegram-bot-api")
+const express = require("express")
+const cors = require("cors")
 
 const token = "5834354543:AAFbJBab2H4dpFg2bx0qvWKXo_i1WeCojjo"
 const webAppUrl = "https://resplendent-dolphin-46b355.netlify.app/"
 
 const bot = new TelegramBot(token, { polling: true })
+const app = express()
+
+app.use(express.json())
+app.use(cors())
 
 bot.on("message", async (msg) => {
     const chatId = msg.chat.id
@@ -29,7 +35,7 @@ bot.on("message", async (msg) => {
 
     if (msg?.web_app_data?.data) {
         try {
-            const data = JSON.parse(msg ? web_app_data?.data)
+            const data = JSON.parse(msg?.web_app_data?.data)
 
             await bot.sendMessage(chatId, "Спасибо за обратную связь!")
             await bot.sendMessage(chatId, "Ваша страна: " + data?.country)
@@ -43,5 +49,31 @@ bot.on("message", async (msg) => {
         }
     }
 })
+
+app.post("/web-data", async (req, res) => {
+    const { queryId, products, totalPrice } = req.body
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: "article",
+            id: queryId,
+            title: "Успешная покупка",
+            input_message_content: { message_text: "Поздравляем с покупкой, Вы приобрели товар на " + totalPrice }
+        })
+        return res.status(200).json({})
+    } catch (e) {
+        await bot.answerWebAppQuery(queryId, {
+            type: "article",
+            id: queryId,
+            title: "Покупка товара не удалась",
+            input_message_content: { message_text: "Покупка товара не удалась" }
+        })
+        return res.status(500).json({})
+    }
+})
+
+
+const PORT = 8000
+
+app.listen(PORT, () => console.log("Server started on PORT " + PORT))
 
 
--- tg-web-app-react/src/components/ProductList/ProductList.jsx
+++ tg-web-app-react/src/components/ProductList/ProductList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect, useCallback } from "react"
 import { useTelegram } from "../hooks/useTelegram"
 import { ProductItem } from "../ProductItem/ProductItem"
 import "./ProductList.css"
@@ -22,7 +22,30 @@ const getTotalPrice = (items = []) => {
 
 const ProductList = () => {
     const [addedItems, setAddedItems] = useState([])
-    const { tg } = useTelegram()
+    const { tg, queryId } = useTelegram()
+
+    const onSendData = useCallback(() => {
+        const data = {
+            products: addedItems,
+            totalPrice: getTotalPrice(addedItems),
+            queryId,
+        }
+        fetch("https://localhost:8000", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        })
+    }, [])
+
+    useEffect(() => {
+        tg.onEvent("mainButtonClicked", onSendData)
+        return () => {
+            tg.offEvent("mainButtonClicked", onSendData)
+        }
+    }, [onSendData])
+
 
     const onAdd = (product) => {
         const alreadyAdded = addedItems.find(item => item.id === product.id)
